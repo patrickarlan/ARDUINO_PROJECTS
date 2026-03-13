d508813480abeb66d4f515cd145812b388003c4b
PatrickArlan <patrickarlanbrequillo@gmail.com>
2026-03-13 17:20:18 +0800

    SETTING UP MY STEPPER MOTOR: - INITIALIZED SETUP AND LOOP - DR: 2, ST: 3

--- STEPPER/src/main.cpp
+++ STEPPER/src/main.cpp
@@ -1,31 +1,15 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-#define STEP_PIN 3
-#define DIR_PIN 2
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
-const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution for your motor
-const int microstep_div = 1;
-const int steps_per_rev_actual = stepsPerRevolution * microstep_div;
+const int DR = 2;
+const int ST = 3;
 
 void setup()
 {
-    stepper.setMaxSpeed(1000);
-    stepper.setAcceleration(500);
+
 }
 
 void loop()
 {
-    stepper.move(steps_per_rev_actual);
-    stepper.runToPosition();
-    delay(1000);
 
-    stepper.move(-(steps_per_rev_actual * 1.5));
-
-    while (stepper.distanceToGo() != 0)
-    {
-        stepper.run();
-    }
-    delay(1000);
 }
